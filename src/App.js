1f9d20934ba73b8ce0e7cf4cf4d53fa3c47740ca
unknown <arne.n.bjerkaas@gmail.com>
2021-02-18 22:02:56 +0100

    Portfolio_part_4: lists

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
 import Projects from "./Projects/Projects";
+import React, { useState } from "react";
 
 function App() {
   const myProjectsArray = ["game", "site", "program"];
+  const [showProjects, setShowProjects] = useState(false);
+
+  const handleShowProjectsButton = () => {
+    setShowProjects(!showProjects);
+  };
+
   return (
     <div className="App">
       <Navbar color="yellow" />
       <Header title="Hello World!" />
-      <Projects projects={myProjectsArray} />
+
+      {/*       <button onClick={handleShowProjectsButton}>Show projects</button>
+      {showProjects === true && <Projects projects={myProjectsArray} />}
+ */}
+      <button onClick={handleShowProjectsButton}>Show Projects</button>
+      {showProjects === true ? (
+        <Projects projects={myProjectsArray} />
+      ) : (
+        <p>Nothing to see here!</p>
+      )}
+
       <Footer color="brown" />
     </div>
   );
